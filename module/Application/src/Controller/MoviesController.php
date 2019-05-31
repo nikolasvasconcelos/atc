8dfbe8e233043359fdcf57e015bb1bdcc1f586c9
nikolas vasconcelos <nkvdamian@gmail.com>
2019-05-31 11:49:16 -0300

    Movies Controller

---
+++ module/Application/src/Controller/MoviesController.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Application\Controller;
+
+use Zend\Http\Request;
+use Zend\Http\Client;
+use Zend\Mvc\Controller\AbstractRestfulController;
+
+class MoviesController extends AbstractRestfulController
+{
+    public function indexAction () {
+        $name = $this->params()->fromQuery("name");
+        if ($name) {
+            $request = new Request();
+            $request->setUri("https://api.themoviedb.org/3/search/movie?api_key=1f54bd990f1cdfb230adb312546d765d&query={$name}");
+            $request->getHeaders()->addHeaders([
+                'Content-Type' => 'application/json',
+                'Content-Type' => 'charset=utf-8'
+            ]);
+            
+            $client = new Client();
+            $response = $client->send($request);
+            
+            echo $response->getBody();
+        } else {
+            echo "Nothing found";
+        }
+    }
+
+    public function getImageAction () {
+        $img = $this->params()->fromQuery("img");
+        if ($img) {
+            $request = new Request();
+            $request->setUri("http://image.tmdb.org/t/p/w500//{$img}");
+            $request->getHeaders()->addHeaders([
+                'Content-Type' => 'image/jpeg',
+            ]);
+            
+            $client = new Client();
+            $response = $client->send($request);
+            
+            return $response;
+        } else {
+            echo "No image found";
+        }
+    }
+}
